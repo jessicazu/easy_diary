a18d4b6344e02f4e0e27299534ec0df8650f9b13
jessicazu <kazu3208o_o@yahoo.co..jp>
2018-06-26 06:37:18 +0900

    [Add] ルーティングを設定した

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  # user
   devise_for :users, controllers: {
     omniauth_callbacks: "omniauth_callbacks"
   }
+
+  # diary
+  resources :diarys, only: [:index, :show]
+  root 'diarys#index'
 end
