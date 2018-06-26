da3141d345345a63f75d80940b03d0c32e185388
jessicazu <kazu3208o_o@yahoo.co..jp>
2018-06-26 10:47:28 +0900

    [Update] LINEログインを実装した

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,2 +1,17 @@
 class OmniauthCallbacksController < ApplicationController
+  def line
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user
+      flash[:notice] = "ログインしました。"
+    else
+      session["devise.line_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def failure
+    redirect_to root_path
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable
          # :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, omniauth_providers: [:line]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name
+      user.image = auth.info.image
+    end
+  end
 end
