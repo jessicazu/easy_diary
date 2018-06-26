a6d189cb25eb41e0b818915962ccbea49a202133
jessicazu <kazu3208o_o@yahoo.co..jp>
2018-06-26 11:41:17 +0900

    [Update] LINEログインのリクエストURLにbot_promptパラメータを追加するようgemを拡張した
    アカウント新規作成時にbotを友達追加する画面を出すため

---
+++ config/initializers/monkey_patches/omniauth-line.rb
@@ -0,0 +1,38 @@
+module OmniAuth
+  module Strategies
+    class Line < OmniAuth::Strategies::OAuth2
+      option :name, 'line'
+      option :scope, 'profile openid'
+
+      option :client_options, {
+        site: 'https://access.line.me',
+        authorize_url: '/oauth2/v2.1/authorize?bot_prompt=aggressive',
+        token_url: '/oauth2/v2.1/token'
+      }
+
+      # host changed
+      def callback_phase
+        options[:client_options][:site] = 'https://api.line.me'
+        super
+      end
+
+      uid { raw_info['userId'] }
+
+      info do
+        {
+          name:        raw_info['displayName'],
+          image:       raw_info['pictureUrl'],
+          description: raw_info['statusMessage']
+        }
+      end
+
+      # Require: Access token with PROFILE permission issued.
+      def raw_info
+        @raw_info ||= JSON.load(access_token.get('v2/profile').body)
+      rescue ::Errno::ETIMEDOUT
+        raise ::Timeout::Error
+      end
+
+    end
+  end
+end
