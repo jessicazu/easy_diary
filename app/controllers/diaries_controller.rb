dbd8dd078da323f45e3465374c7c102b7458412b
jessicazu <kazu3208o_o@yahoo.co..jp>
2018-06-29 17:37:00 +0900

    [Update] diariesコントローラーを編集した

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,48 +1,26 @@
 class DiariesController < ApplicationController
-  require 'line/bot'
-
   before_action :authenticate_user!
-  protect_from_forgery except: [:callback]
 
   def index
-  end
+    # 現在のユーザーの日記
+    diaries = current_user.diaries
+    # ユーザーの登録日から、本日までの日付の範囲
+    days = (Date.parse(current_user.created_at.strftime('%F'))..Date.today)
 
-  def callback
-    body = request.body.read
-
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      error 400 do 'Bad Request' end
+    # daysに対して、その日付の日記データがあれば、日付データと置き換える
+    datas = days.map do |day|
+      if diary = diaries.find { |d| Date.parse(d.created_at.strftime('%F')) == day }
+        diary
+      else
+        day
+      end
     end
 
-    events = client.parse_events_from(body)
-
-    events.each { |event|
-      case event
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          text = event.message['text']
-          message = {
-            type: 'text',
-            text: text
-          }
-          client.reply_message(event['replyToken'], message)
-        end
-      end
-    }
-
-    head :ok
+    # ページネーションを実装
+    @datas = Kaminari.paginate_array(datas.reverse).page(params[:page]).per(20)
   end
 
   def show
   end
 
-  private
-  def client
-    @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
-  end
 end
