7ab0127d85d2a2d75b462aef52e48a83cccfd9da
jessicazu <kazu3208o_o@yahoo.co..jp>
2018-06-29 11:00:18 +0900

    [Add] アソシエーションを追加した

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable
          # :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, omniauth_providers: [:line]
+  has_many :diaries
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
