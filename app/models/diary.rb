c34dd3bba16018bece09f07ee07eba034ab67dc2
jessicazu <kazu3208o_o@yahoo.co..jp>
2018-06-30 09:33:35 +0900

    [Add] Diaryモデルにfeeling_japaneseメソッドを追加した

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,3 +1,14 @@
 class Diary < ApplicationRecord
   belongs_to :user
+
+  def feeling_japanese
+    case feeling
+    when "good"
+      "良かった"
+    when "normal"
+      "普通"
+    when "bad"
+      "悪かった"
+    end
+  end
 end
